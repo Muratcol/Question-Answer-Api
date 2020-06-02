99cc734b669158b0789cf52c4c407bc0c6aa8f31
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-06-02 18:27:52 +0300

    Demo Version Helpers Added

--- helpers/currencyScrapper/currency_finder.js
+++ helpers/currencyScrapper/currency_finder.js
@@ -1,123 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const asyncErrorWrapper = require("express-async-handler");
+const cheerio = require("cheerio");
+const axios = require("axios");
+let allCurrencies = new Array();
 
+const getAllCurrencies = asyncErrorWrapper(async (req, res, next) => {
+  let url = "https://kur.doviz.com/";
+  let response;
 
-const connectDatabase = require("../database/connectDatabase");
-const Currency = require("../../models/Currency");
-
-
-
-
-// const Scrapping = async () => {
-//     const response = await axios.get("https://kur.doviz.com/");
-//     const $ = await cheerio.load(response.data);
-//     let denemeler = $('#currencies tbody tr');
-//     let bigdata = new Array
-//     for (let i = 1; i < denemeler.length+1 ; i++) {
-//         if ((i === 12) | (i === 6)) continue
-//         const name = $(`tr:nth-child(${i}) > td:nth-child(1)`).text().trim()
-//         const alis = $(`tr:nth-child(${i}) > td:nth-child(2)`).text().trim()
-//         const satis = $(`tr:nth-child(${i}) > td:nth-child(3)`).text().trim()
-//         const yuksek = $(`tr:nth-child(${i}) > td:nth-child(4)`).text().trim()
-//         const dusuk = $(`tr:nth-child(${i}) > td:nth-child(5)`).text().trim()
-//         const degisim = $(`tr:nth-child(${i}) > td:nth-child(6)`).text().trim()
-//         const update = $(`tr:nth-child(${i}) > td:nth-child(7)`).text().trim()
-//         bigdata.push({
-//             name: name,
-//             alis: alis,
-//             satis: satis,
-//             yuksek: yuksek,
-//             dusuk, dusuk,
-//             degisim: degisim,
-//             update: update
-//         })
-//     }
-//     // connectDatabase();
-//     // Currency.create(bigdata);
-//     // return bigdata
-//     return await bigdata
-// }
-    
-
-const Scrapping = asyncErrorWrapper( async function() {
-    let url = "https://kur.doviz.com/";
-    let response;
-    try{
-      response = await axios.get(url);
-      const $ = cheerio.load(response.data)
-      let denemeler = $('#currencies tbody tr')    
-      for (let i = 1; i < denemeler.length+1 ; i++) {
-          if ((i === 12) | (i === 6)) continue
-          const name = $(`tr:nth-child(${i}) > td:nth-child(1)`).text().trim();
-          const alis = $(`tr:nth-child(${i}) > td:nth-child(2)`).text().trim();
-          const satis = $(`tr:nth-child(${i}) > td:nth-child(3)`).text().trim();
-          const yuksek = $(`tr:nth-child(${i}) > td:nth-child(4)`).text().trim();
-          const dusuk = $(`tr:nth-child(${i}) > td:nth-child(5)`).text().trim();
-          const degisim = $(`tr:nth-child(${i}) > td:nth-child(6)`).text().trim();
-          const update = $(`tr:nth-child(${i}) > td:nth-child(7)`).text().trim();
-          bigdata.push({
-              name: name,
-              alis: alis,
-              satis: satis,
-              yuksek: yuksek,
-              dusuk, dusuk,
-              degisim: degisim,
-              update: update
-          })};
-          return bigdata
-    }
-    catch(err) {
-      return res.status(500)
-
-    }
-})
-  
-// console.log(Scrapping())
+  response = await axios.get(url);
+  const $ = await cheerio.load(response.data);
+  let denemeler = await $("#currencies tbody tr");
+  for (let i = 1; i < denemeler.length + 1; i++) {
+    if ((i === 12) | (i === 6)) continue;
+    const name = $(`tr:nth-child(${i}) > td:nth-child(1)`).text().trim();
+    const alis = $(`tr:nth-child(${i}) > td:nth-child(2)`).text().trim();
+    const satis = $(`tr:nth-child(${i}) > td:nth-child(3)`).text().trim();
+    const yuksek = $(`tr:nth-child(${i}) > td:nth-child(4)`).text().trim();
+    const dusuk = $(`tr:nth-child(${i}) > td:nth-child(5)`).text().trim();
+    const degisim = $(`tr:nth-child(${i}) > td:nth-child(6)`).text().trim();
+    const update = $(`tr:nth-child(${i}) > td:nth-child(7)`).text().trim();
+    allCurrencies.push({
+      name: name,
+      alis: alis,
+      satis: satis,
+      yuksek: yuksek,
+      dusuk,
+      dusuk,
+      degisim: degisim,
+      update: update,
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    data: allCurrencies,
+  });
+});
 
 module.exports = {
-  Scrapping
+  getAllCurrencies,
 };
-
-
-
-// const Scrappin = new Promise((resolve) => {
-//     axios.get("https://kur.doviz.com/").then((response) => {
-//     const $ = cheerio.load(response.data)
-//     let denemeler = $('#currencies tbody tr')    
-//     for (let i = 1; i < denemeler.length+1 ; i++) {
-//         if ((i === 12) | (i === 6)) continue
-//         const name = $(`tr:nth-child(${i}) > td:nth-child(1)`).text().trim()
-//         const alis = $(`tr:nth-child(${i}) > td:nth-child(2)`).text().trim()
-//         const satis = $(`tr:nth-child(${i}) > td:nth-child(3)`).text().trim()
-//         const yuksek = $(`tr:nth-child(${i}) > td:nth-child(4)`).text().trim()
-//         const dusuk = $(`tr:nth-child(${i}) > td:nth-child(5)`).text().trim()
-//         const degisim = $(`tr:nth-child(${i}) > td:nth-child(6)`).text().trim()
-//         const update = $(`tr:nth-child(${i}) > td:nth-child(7)`).text().trim()
-//         bigdata.push({
-//             name: name,
-//             alis: alis,
-//             satis: satis,
-//             yuksek: yuksek,
-//             dusuk, dusuk,
-//             degisim: degisim,
-//             update: update
-//         })
-//     }
-//     resolve(bigdata)
-//     })
-// });   
-
-
-
-
-
-
-
-
-
-  // Print the full HTML
-// console.log(`Site HTML: ${$.html()}\n\n`)
-  
-  // Print some specific page content
-
-// document.querySelector("#currencies > tbody > tr:nth-child(1) > td:nth-child(2)")
-// document.querySelector("#currencies > tbody > tr:nth-child(1)")
-/* <td style="text-align:right;" class="">6,8993</td> */
--- routers/currency.js
+++ routers/currency.js
@@ -1,45 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const asyncErrorWrapper = require("express-async-handler");
-const cheerio = require("cheerio");
-const axios = require("axios");
-let allCurrencies = new Array();
-// const {getAllCurrencies}
 
-router.get(
-  "/",
-  asyncErrorWrapper(async (req, res) => {
-    let url = "https://kur.doviz.com/";
-    let response;
 
-    response = await axios.get(url);
-    const $ = await cheerio.load(response.data);
-    let denemeler = await $("#currencies tbody tr");
-    for (let i = 1; i < denemeler.length + 1; i++) {
-      if ((i === 12) | (i === 6)) continue;
-      const name = $(`tr:nth-child(${i}) > td:nth-child(1)`).text().trim();
-      const alis = $(`tr:nth-child(${i}) > td:nth-child(2)`).text().trim();
-      const satis = $(`tr:nth-child(${i}) > td:nth-child(3)`).text().trim();
-      const yuksek = $(`tr:nth-child(${i}) > td:nth-child(4)`).text().trim();
-      const dusuk = $(`tr:nth-child(${i}) > td:nth-child(5)`).text().trim();
-      const degisim = $(`tr:nth-child(${i}) > td:nth-child(6)`).text().trim();
-      const update = $(`tr:nth-child(${i}) > td:nth-child(7)`).text().trim();
-      allCurrencies.push({
-        name: name,
-        alis: alis,
-        satis: satis,
-        yuksek: yuksek,
-        dusuk,
-        dusuk,
-        degisim: degisim,
-        update: update,
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      data: allCurrencies,
-    });
-  })
-);
+const {
+  getAllCurrencies,
+} = require("../helpers/currencyScrapper/currency_finder");
+
+router.get("/", getAllCurrencies);
 
 module.exports = router;
